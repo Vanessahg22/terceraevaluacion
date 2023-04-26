b50bc00e0214616571b2d16a2a57bd4e56c52970
Vanessa Herrero <113891031+Vanessahg22@users.noreply.github.com>
2023-04-26 12:44:44 +0200

    Create Herrero_Vanessa_ejercicio_1.java

---
+++ Herrero_Vanessa_ejercicio_1.java
@@ -0,0 +1,24 @@
+package PrimerosProgramas;
+public class Cilindro {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		private boolean tieneFormaCilindrica;
+		public tieneFormaCilindrica (String formaCilindro, int tamano, int area, int color, boolean tieneFormaCilindrica) {
+			super(formaCilindro, tamano, area, color);
+			this.tieneFormaCilindrica=tieneFormaCilindrica;
+			// TODO Auto-generated constructor stub
+		}
+		public boolean isTieneFormaCilindrica() {
+			return tieneFormaCilindrica;
+		}
+		public void setTieneFormaCilindrica(boolean tieneFormaCilindrica) {
+			this.tieneFormaCilindrica = tieneFormaCilindrica;
+		}
+
+
+	}
+
+	}
+
+}
