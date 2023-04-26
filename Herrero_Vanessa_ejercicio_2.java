e1be6f5597b673ea1b30e4e395eb2d7c5e00e716
Vanessa Herrero <113891031+Vanessahg22@users.noreply.github.com>
2023-04-26 12:46:39 +0200

    Create Herrero_Vanessa_ejercicio_2.java

---
+++ Herrero_Vanessa_ejercicio_2.java
@@ -0,0 +1,74 @@
+package PrimerosProgramas;
+public class Vector {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		public class OperacionesVectores {
+			
+			public static void main(String[]args) {
+				//TODO Auto-generated method stub
+				int x;
+				int y;
+				int z;
+				x=5;
+				y=6;
+				z=2
+				System.out.print(x+y+z);
+			}
+			public static void main(String[]args) {
+				//TODO Auto-generated method stub
+				int x;
+				int y;
+				int z;
+				x=5;
+				y=6;
+				z=2
+				System.out.print(x-y-z);
+			}
+			public static void main(String[]args) {
+				//TODO Auto-generated method stub
+				int x;
+				int y;
+				int z;
+				x=5;
+				y=6;
+				z=2
+			    float x,y,z=u
+			    
+			    int w;
+			    int t;
+			    int r;
+			    w=8
+			    t=3
+			    r=1
+			    float w,t,r=v
+				
+				System.out.print(u·v);
+			}
+			    public static void main(String[]args) {
+					//TODO Auto-generated method stub
+					int x;
+					int y;
+					int z;
+					x=5;
+					y=6;
+					z=2
+				    float x,y,z=u
+				    
+				    int w;
+				    int t;
+				    int r;
+				    w=8
+				    t=3
+				    r=1
+				    float w,t,r=v
+					
+					System.out.print(u x v);
+			    
+			}
+		}
+
+	}
+
+}
